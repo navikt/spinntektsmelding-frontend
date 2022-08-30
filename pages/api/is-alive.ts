53835f28f5da5f4ce743b08e485f39f9c23ec685
Kent Fjellstad <kent.fjellstad@nav.no>
2022-08-30 14:00:33 +0200

    Tester andre pather

--- pages/api/is-alive.ts
+++ pages/api/is-alive.ts
@@ -2,5 +2,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  res.status(200).json('is-alive');
+  res.status(200).send('is-alive');
 }
--- pages/api/is-ready.ts
+++ pages/api/is-ready.ts
@@ -2,5 +2,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  res.status(200).json('is-ready');
+  res.status(200).send('is-ready');
 }
