7c4afee1bb3d1a6104747352567c8815ca22ab1b
Kent Fjellstad <kent.fjellstad@nav.no>
2022-10-25 10:42:06 +0200

    Bruker backend

--- pages/api/inntektsmelding.ts
+++ pages/api/inntektsmelding.ts
@@ -43,16 +43,16 @@ export const config = {
 const handler = (
   req: NextApiRequest,
   res: NextApiResponse<Data> //res.status(200).json(org);
-) => res.status(200).json(org);
-// httpProxyMiddleware(req, res, {
-//   target: basePath,
-//   onProxyInit: handleProxyInit,
-//   pathRewrite: [
-//     {
-//       patternStr: '^/api/inntektsmelding',
-//       replaceStr: ''
-//     }
-//   ]
-// });
+) =>
+  httpProxyMiddleware(req, res, {
+    target: basePath,
+    onProxyInit: handleProxyInit,
+    pathRewrite: [
+      {
+        patternStr: '^/api/inntektsmelding',
+        replaceStr: ''
+      }
+    ]
+  });
 
 export default handler;
