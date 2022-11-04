042c17dc116143006e9f2ef6c4c4b402cdb1de1b
Kent Fjellstad <kent.fjellstad@nav.no>
2022-11-04 14:50:09 +0100

    Tester

--- __tests__/utils/formatIsoDate.test.ts
+++ __tests__/utils/formatIsoDate.test.ts
@@ -8,4 +8,8 @@ describe('formatIsoDate', () => {
   it('shold return an empty string when input is rubish', () => {
     expect(formatIsoDate()).toBe('');
   });
+
+  it('shold return a blank when the date is illegal', () => {
+    expect(formatIsoDate(new Date(-9, -13 / 0, -18))).toBe('');
+  });
 });
