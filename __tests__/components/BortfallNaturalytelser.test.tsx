55cc9fbe98d342f89aadc328d9172f6e8fd737c6
Kent Fjellstad <kent.fjellstad@nav.no>
2023-03-03 10:42:36 +0100

    Test av bortfall naturalytelser visningskomponent

---
+++ __tests__/components/BortfallNaturalytelser.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import BortfallNaturalytelser from '../../components/BortfallNaturalytelser/BortfallNaturalytelser';
+import { Naturalytelse } from '../../state/state';
+
+describe('BortfallNaturalytelser', () => {
+  it('renders a text', () => {
+    const ytelser: Array<Naturalytelse> = [];
+    render(<BortfallNaturalytelser ytelser={ytelser} />);
+
+    const buttonTitle = screen.getByText('Ingen naturalytelser har falt bort');
+    expect(buttonTitle).toBeInTheDocument();
+  });
+
+  it('should have no violations', async () => {
+    const ytelser: Array<Naturalytelse> = [];
+    const { container } = render(<BortfallNaturalytelser ytelser={ytelser} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+
+  it('renders a text', () => {
+    const ytelser: Array<Naturalytelse> = [
+      { id: '1', type: 'friTransport', bortfallsdato: new Date(2022, 2, 2), verdi: 1234.5 },
+      { id: '2', type: 'kostbesparelseIHjemmet', bortfallsdato: new Date(2022, 2, 2), verdi: 1234.5 }
+    ];
+    render(<BortfallNaturalytelser ytelser={ytelser} />);
+
+    const transport = screen.getByText('Fri transport');
+    expect(transport).toBeInTheDocument();
+
+    const kost = screen.getByText('Kostbesparelse i hjemmet');
+    expect(kost).toBeInTheDocument();
+  });
+
+  it('should have no violations', async () => {
+    const ytelser: Array<Naturalytelse> = [
+      { id: '1', type: 'friTransport', bortfallsdato: new Date(2022, 2, 2), verdi: 1234.5 }
+    ];
+
+    const { container } = render(<BortfallNaturalytelser ytelser={ytelser} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
