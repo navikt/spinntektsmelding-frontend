433b100a86e809ec9e937155061b98069a272d0d
Kent Fjellstad <kent.fjellstad@nav.no>
2023-03-07 10:08:19 +0100

    Bommet litt med testdata igjen

--- mockdata/formData.ts
+++ mockdata/formData.ts
@@ -164,7 +164,7 @@ const formData = {
     },
     {
       maanedsnavn: '2023-01', // yyyy-MM
-      inntekt: 88000
+      inntekt: 66000
     },
     {
       maanedsnavn: '2022-12', // yyyy-MM
@@ -172,7 +172,7 @@ const formData = {
     },
     {
       maanedsnavn: '2022-11', // yyyy-MM
-      inntekt: 66000
+      inntekt: 88000
     },
     {
       maanedsnavn: '2022-10', // yyyy-MM
