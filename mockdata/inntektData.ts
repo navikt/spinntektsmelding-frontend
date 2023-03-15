07813a4bdd18dcf50f6660c9ae0f1db92c8b38c2
Kent Fjellstad <kent.fjellstad@nav.no>
2023-03-15 13:26:18 +0100

    MockData

---
+++ mockdata/inntektData.ts
@@ -0,0 +1,19 @@
+const inntektData = {
+  bruttoinntekt: 84333.33,
+  tidligereInntekter: [
+    {
+      maanedsnavn: '2022-12',
+      inntekt: 88000.0
+    },
+    {
+      maanedsnavn: '2022-11',
+      inntekt: 88000.0
+    },
+    {
+      maanedsnavn: '2022-10',
+      inntekt: 77000.0
+    }
+  ]
+};
+
+export default inntektData;
