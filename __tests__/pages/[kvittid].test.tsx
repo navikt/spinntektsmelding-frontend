a36858cd084a066eca59132936e10f6423035815
Kent Fjellstad <kent.fjellstad@nav.no>
2023-03-20 10:54:09 +0100

    Veldig enkel a11y test av kvitteringen

---
+++ __tests__/pages/[kvittid].test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import { vi } from 'vitest';
+import Kvittering from '../../pages/kvittering/[kvittid]';
+
+vi.mock('next/router', () => require('next-router-mock'));
+
+describe('kvittering', () => {
+  it('renders a title text', () => {
+    const spy = vi.spyOn(window, 'print');
+
+    render(<Kvittering />);
+
+    const buttonTitle = screen.getByRole('button', {
+      name: /Skriv ut/i
+    });
+
+    buttonTitle.click();
+
+    expect(spy).toHaveBeenCalled();
+
+    expect(buttonTitle).toBeInTheDocument();
+  });
+
+  it('should have no violations', async () => {
+    const { container } = render(<Kvittering />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
