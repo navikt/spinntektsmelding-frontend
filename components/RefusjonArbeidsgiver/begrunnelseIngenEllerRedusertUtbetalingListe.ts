a99eaab06bbef83280715528f109118bc9511da5
Kent Fjellstad <kent.fjellstad@nav.no>
2023-03-28 14:47:44 +0200

    Begrunnelsesliste ingen eller redusert utbetaling

--- components/RefusjonArbeidsgiver/begrunnelseIngenEllerRedusertUtbetalingListe.ts
+++ components/RefusjonArbeidsgiver/begrunnelseIngenEllerRedusertUtbetalingListe.ts
@@ -3,21 +3,22 @@ interface Begrunnelser {
 }
 
 const begrunnelseIngenEllerRedusertUtbetalingListe: Begrunnelser = {
-  LovligFravaer: 'Lovlig fravær',
-  FravaerUtenGyldigGrunn: 'Fravær uten gyldig grunn',
-  ArbeidOpphoert: 'Arbeid opphørt',
-  BeskjedGittForSent: 'Beskjed gitt for sent',
-  ManglerOpptjening: 'Mangler opptjening',
-  IkkeLoenn: 'Ikke lønn',
-  BetvilerArbeidsufoerhet: 'Betviler arbeidsuførhet',
-  IkkeFravaer: 'Ikke fravær',
+  LovligFravaer: 'Lovlig fravær uten lønn ',
+  FravaerUtenGyldigGrunn: 'Ikke lovlig fravær',
+  ArbeidOpphoert: 'Arbeidsforholdet er avsluttet',
+  BeskjedGittForSent: 'Beskjed om fravær gitt for sent eller sykmeldingen er ikke sendt i tide',
+  ManglerOpptjening: 'Det er ikke fire ukers opptjeningstid',
+  IkkeLoenn: 'Det er ikke avtale om videre arbeid',
+  BetvilerArbeidsufoerhet: 'Vi betviler at ansatt er ute av stand til å jobbe',
+  IkkeFravaer: 'Ansatt har ikke hatt fravær fra jobb',
   StreikEllerLockout: 'Streikk eller lockout',
-  Permittering: 'Permitering',
-  FiskerMedHyre: 'Fisker med hyre',
-  Saerregler: 'Særregler',
-  FerieEllerAvspasering: 'Ferie eller avspassering',
-  IkkeFullStillingsandel: 'Ikke full stillingsandel',
-  TidligereVirksomhet: 'Tidligere virksomhet'
+  Permittering: 'Ansatt er helt eller delvis permittert',
+  FiskerMedHyre: 'Ansatt er fisker med hyre på blad B',
+  Saerregler: 'Ansatt skal være donor eller skal til kontrollundersøkelse som varer i mer enn 24 timer',
+  FerieEllerAvspasering:
+    'Mindre enn 16 dager siden arbeidet ble gjenopptatt på grunn av lovpålagt ferie eller avspasering',
+  IkkeFullStillingsandel: 'Ansatt har ikke gjenopptatt full stilling etter forrige arbeidsgiverperiode',
+  TidligereVirksomhet: 'Arbeidsgiverperioden er helt eller delvis gjennomført hos tidligere virksomhet '
 };
 
 export default begrunnelseIngenEllerRedusertUtbetalingListe;
