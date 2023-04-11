255a1d86c21bbd5a9ffdc86fe96ef7448a3d3f5e
Kent Fjellstad <kent.fjellstad@nav.no>
2023-04-11 13:35:24 +0200

    Rettet test

--- __tests__/utils/formatBegrunnelseEndringBruttoinntekt.test.ts
+++ __tests__/utils/formatBegrunnelseEndringBruttoinntekt.test.ts
@@ -10,7 +10,7 @@ describe.concurrent('formatBegrunnelseEndringBruttoinntekt', () => {
   });
 
   it('shold return VarigLonnsendring', () => {
-    expect(formatBegrunnelseEndringBruttoinntekt('VarigLonnsendring')).toBe('VarigLonnsendring');
+    expect(formatBegrunnelseEndringBruttoinntekt('VarigLonnsendring')).toBe('Varig lønnsendring');
   });
 
   it('shold return Permisjon', () => {
