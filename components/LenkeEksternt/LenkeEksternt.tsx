fda8b1af51b5011f180f8a8dabc73f5dc26cf654
Kent Fjellstad <kent.fjellstad@nav.no>
2023-04-17 19:22:27 +0200

    Ugyldig lenke styling

--- components/LenkeEksternt/LenkeEksternt.tsx
+++ components/LenkeEksternt/LenkeEksternt.tsx
@@ -7,7 +7,7 @@ export default function LenkeEksternt(props: LenkeEksterntProps) {
   const { isHidden, ...restProps } = props;
 
   return (
-    <a target='_blank' rel='noopener noreferrer' {...restProps} tabIndex={tabIndex}>
+    <a target='_blank' rel='noopener noreferrer' {...restProps} tabIndex={tabIndex} className='navds-link'>
       {props.children}
     </a>
   );
