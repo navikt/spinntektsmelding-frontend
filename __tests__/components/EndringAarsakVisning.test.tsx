9ddc8f2f1c22a531b76eaa89e045ff5d064541c2
Kent Fjellstad <kent.fjellstad@nav.no>
2023-04-20 08:55:35 +0200

    Test av EndringAarsakVisning (#97)
    * Test av EndringAarsakVisning

---
+++ __tests__/components/EndringAarsakVisning.test.tsx
@@ -0,0 +1,114 @@
+import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
+import EndringAarsakVisning from '../../components/EndringAarsakVisning/EndringAarsakVisning';
+
+describe('EndringAarsakVisning', () => {
+  it('should show tariffendringsdata.', async () => {
+    const { container } = render(
+      <EndringAarsakVisning
+        endringsaarsak={'Tariffendring'}
+        tariffendringDato={new Date(2022, 10, 10)}
+        tariffkjentdato={new Date(2022, 10, 15)}
+      />
+    );
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+
+    expect(await screen.findByText(/Tariffendring gjelder fra:/)).toBeInTheDocument();
+    expect(await screen.findByText(/10.11.2022/)).toBeInTheDocument();
+    expect(await screen.findByText(/Dato tariffendring ble kjent:/)).toBeInTheDocument();
+    expect(await screen.findByText(/15.11.2022/)).toBeInTheDocument();
+  });
+
+  it('should show ferie data.', async () => {
+    const perioder = [
+      { fom: new Date(2022, 10, 10), tom: new Date(2022, 10, 15), id: '1' },
+      { fom: new Date(2022, 11, 10), tom: new Date(2022, 11, 15), id: '2' }
+    ];
+    const { container } = render(<EndringAarsakVisning endringsaarsak={'Ferie'} ferie={perioder} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+
+    expect(await screen.findAllByText(/Fra/)).toHaveLength(2);
+    expect(await screen.findByText(/10.11.2022/)).toBeInTheDocument();
+    expect(await screen.findAllByText(/Til/)).toHaveLength(2);
+    expect(await screen.findByText(/15.11.2022/)).toBeInTheDocument();
+    expect(await screen.findByText(/10.12.2022/)).toBeInTheDocument();
+    expect(await screen.findByText(/15.12.2022/)).toBeInTheDocument();
+  });
+
+  it('should show VarigLonnsendring data.', async () => {
+    const { container } = render(
+      <EndringAarsakVisning endringsaarsak={'VarigLonnsendring'} lonnsendringsdato={new Date(2022, 11, 15)} />
+    );
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+
+    expect(await screen.findByText(/Varig lønnsendringsdato/)).toBeInTheDocument();
+    expect(await screen.findByText(/15.12.2022/)).toBeInTheDocument();
+  });
+
+  it('should show permisjon data.', async () => {
+    const perioder = [
+      { fom: new Date(2022, 10, 10), tom: new Date(2022, 10, 15), id: '1' },
+      { fom: new Date(2022, 11, 10), tom: new Date(2022, 11, 15), id: '2' }
+    ];
+    const { container } = render(<EndringAarsakVisning endringsaarsak={'Permisjon'} permisjon={perioder} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+
+    expect(await screen.findAllByText(/Fra/)).toHaveLength(2);
+    expect(await screen.findByText(/10.11.2022/)).toBeInTheDocument();
+    expect(await screen.findAllByText(/Til/)).toHaveLength(2);
+    expect(await screen.findByText(/15.11.2022/)).toBeInTheDocument();
+    expect(await screen.findByText(/10.12.2022/)).toBeInTheDocument();
+    expect(await screen.findByText(/15.12.2022/)).toBeInTheDocument();
+  });
+
+  it('should show permitering data.', async () => {
+    const perioder = [
+      { fom: new Date(2022, 10, 10), tom: new Date(2022, 10, 15), id: '1' },
+      { fom: new Date(2022, 11, 10), tom: new Date(2022, 11, 15), id: '2' }
+    ];
+    const { container } = render(<EndringAarsakVisning endringsaarsak={'Permittering'} permittering={perioder} />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+
+    expect(await screen.findAllByText(/Fra/)).toHaveLength(2);
+    expect(await screen.findByText(/10.11.2022/)).toBeInTheDocument();
+    expect(await screen.findAllByText(/Til/)).toHaveLength(2);
+    expect(await screen.findByText(/15.11.2022/)).toBeInTheDocument();
+    expect(await screen.findByText(/10.12.2022/)).toBeInTheDocument();
+    expect(await screen.findByText(/15.12.2022/)).toBeInTheDocument();
+  });
+
+  it('should show NyStilling.', async () => {
+    const { container } = render(
+      <EndringAarsakVisning endringsaarsak={'NyStilling'} nystillingdato={new Date(2022, 10, 10)} />
+    );
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+
+    expect(await screen.findByText(/Ny stilling fra/)).toBeInTheDocument();
+    expect(await screen.findByText(/10.11.2022/)).toBeInTheDocument();
+  });
+
+  it('should show NyStillingsprosent.', async () => {
+    const { container } = render(
+      <EndringAarsakVisning endringsaarsak={'NyStillingsprosent'} nystillingsprosentdato={new Date(2022, 10, 10)} />
+    );
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+
+    expect(await screen.findByText(/Ny stillingsprosent fra/)).toBeInTheDocument();
+    expect(await screen.findByText(/10.11.2022/)).toBeInTheDocument();
+  });
+});
