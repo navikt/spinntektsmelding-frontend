1e15c8f993b6d8b47490911014c04879774af09b
Kent Fjellstad <kent.fjellstad@nav.no>
2023-06-12 10:47:13 +0200

    Sonar

--- state/useFyllInnsending.ts
+++ state/useFyllInnsending.ts
@@ -194,7 +194,7 @@ export default function useFyllInnsending() {
     setSkalViseFeilmeldinger(true);
     let perioder;
     if (fravaersperioder) {
-      perioder = fravaersperioder.concat(egenmeldingsperioder);
+      perioder = fravaersperioder.concat(egenmeldingsperioder ?? []);
     } else {
       perioder = egenmeldingsperioder;
     }
@@ -202,18 +202,17 @@ export default function useFyllInnsending() {
     const innsendbarArbeidsgiverperioder: Array<SendtPeriode> | undefined =
       finnInnsendbareArbeidsgiverperioder(arbeidsgiverperioder);
 
+    const formatertePerioder = innsendbarArbeidsgiverperioder
+      ? innsendbarArbeidsgiverperioder?.map((periode) => ({
+          fom: parseISO(periode.fom),
+          tom: parseISO(periode.tom),
+          id: 'id'
+        }))
+      : undefined;
+
     const bestemmendeFraværsdag = bestemmendeFravaersdag
       ? formatIsoDate(bestemmendeFravaersdag)
-      : finnBestemmendeFravaersdag(
-          perioder,
-          innsendbarArbeidsgiverperioder
-            ? innsendbarArbeidsgiverperioder?.map((periode) => ({
-                fom: parseISO(periode.fom),
-                tom: parseISO(periode.tom),
-                id: 'id'
-              }))
-            : undefined
-        );
+      : finnBestemmendeFravaersdag(perioder, formatertePerioder);
 
     const aarsakInnsending = nyInnsending ? 'Ny' : 'Endring'; // Kan være Ny eller Endring
 
@@ -307,7 +306,7 @@ function konverterRefusjonsendringer(
     : undefined;
 }
 
-function sjekkOmViHarEgenmeldingsdager(egenmeldingsperioder: Array<Periode>) {
+function sjekkOmViHarEgenmeldingsdager(egenmeldingsperioder: Array<Periode> | undefined) {
   return (
     egenmeldingsperioder &&
     (egenmeldingsperioder.length > 1 || (egenmeldingsperioder[0]?.fom && egenmeldingsperioder[0]?.tom))
