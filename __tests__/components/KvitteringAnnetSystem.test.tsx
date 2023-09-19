09185ac8a4bc1689a8745d3039a4a18a392259ac
Kent Fjellstad <kent.fjellstad@nav.no>
2023-09-19 12:52:16 +0200

    En test

---
+++ __tests__/components/KvitteringAnnetSystem.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import KvitteringAnnetSystem from '../../components/KvitteringAnnetSystem/KvitteringAnnetSystem';
+
+describe('KvitteringAnnetSystem', () => {
+  it('should render the component with the correct props', () => {
+    const props = {
+      arkivreferanse: '123456',
+      lenkeTilKvittering: 'Kvittering',
+      lenkeTilKvitteringHref: 'https://example.com/kvittering',
+      ekstertSystem: 'Eksternt system',
+      mottattDato: '2022-01-01'
+    };
+
+    const { getByText } = render(<KvitteringAnnetSystem {...props} />);
+
+    expect(getByText('Mottatt inntektsmelding')).toBeInTheDocument();
+    expect(getByText('Denne inntektsmeldingen er mottatt fra et eksternt system')).toBeInTheDocument();
+    expect(getByText(/Vi har mottatt denne inntektsmeldingen fra Eksternt system/)).toBeInTheDocument();
+    expect(getByText(`Arkivrefferanse: [${props.arkivreferanse}]`)).toBeInTheDocument();
+    expect(getByText(props.lenkeTilKvittering)).toHaveAttribute('href', props.lenkeTilKvitteringHref);
+  });
+
+  it('should render the component without optional props', () => {
+    const props = {
+      ekstertSystem: 'Eksternt system'
+    };
+
+    const { getByText, queryByText } = render(<KvitteringAnnetSystem {...props} />);
+
+    expect(getByText('Mottatt inntektsmelding')).toBeInTheDocument();
+    expect(getByText('Denne inntektsmeldingen er mottatt fra et eksternt system')).toBeInTheDocument();
+    expect(getByText(/Vi har mottatt denne inntektsmeldingen fra Eksternt system/)).toBeInTheDocument();
+    expect(queryByText('Arkivrefferanse:')).toBeNull();
+    expect(queryByText('Eventuell lenke til kvittering')).toBeNull();
+  });
+});
