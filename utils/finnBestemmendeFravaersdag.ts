46e8d5bcc3cccce34eb6df333447d9bf1cb807da
Kent Fjellstad <kent.fjellstad@nav.no>
2023-10-17 12:01:37 +0200

    Fjerner console.log

--- utils/finnBestemmendeFravaersdag.ts
+++ utils/finnBestemmendeFravaersdag.ts
@@ -105,13 +105,12 @@ const finnBestemmendeFravaersdag = (
     )
       ? forespurtBestemmendeFraværsdag
       : bestemmendeFravaersdag;
-    console.log('forespurtBestemmendeFravaersdagErStorst', forespurtBestemmendeFravaersdagErStorst);
+
     return formatISO9075(forespurtBestemmendeFravaersdagErStorst as Date, {
       representation: 'date'
     });
   }
 
-  console.log('bestemmendeFravaersdag', bestemmendeFravaersdag);
   if (bestemmendeFravaersdag !== undefined) {
     return formatISO9075(bestemmendeFravaersdag, {
       representation: 'date'
