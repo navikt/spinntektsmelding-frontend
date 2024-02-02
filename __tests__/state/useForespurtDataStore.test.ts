b4e01a8712da5a1865a7d4fba5d27cc666069f00
Kent Fjellstad <kent.fjellstad@nav.no>
2024-02-02 10:01:03 +0100

    Tester

--- __tests__/state/useForespurtDataStore.test.ts
+++ __tests__/state/useForespurtDataStore.test.ts
@@ -204,4 +204,129 @@ describe('useForespurtDataStore', () => {
 
     expect(typer).toEqual(input);
   });
+
+  it('should return false for arbeidsgiverKreverRefusjon', () => {
+    const { result } = renderHook(() => useBoundStore((state) => state));
+
+    let typer;
+    act(() => {
+      typer = result.current.arbeidsgiverKreverRefusjon();
+    });
+
+    expect(typer).toBeFalsy();
+  });
+
+  it('should return false for arbeidsgiverRefusjonskravHarEndringer', () => {
+    const { result } = renderHook(() => useBoundStore((state) => state));
+
+    let typer;
+    act(() => {
+      typer = result.current.arbeidsgiverRefusjonskravHarEndringer();
+    });
+
+    expect(typer).toBeFalsy();
+  });
+
+  it('should return false for arbeidsgiverRefusjonskravOpphører', () => {
+    const { result } = renderHook(() => useBoundStore((state) => state));
+
+    let typer;
+    act(() => {
+      typer = result.current.arbeidsgiverRefusjonskravOpphører();
+    });
+
+    expect(typer).toBeFalsy();
+  });
+
+  it('should return if arbeidsgiverKreverRefusjon', () => {
+    const { result } = renderHook(() => useBoundStore((state) => state));
+
+    act(() => {
+      result.current.initForespurtData({
+        arbeidsgiverperiode: { paakrevd: false },
+        inntekt: {
+          paakrevd: true,
+          forslag: {
+            type: 'ForslagInntektGrunnlag',
+            beregningsmaaneder: ['2023-05', '2023-06', '2023-07']
+          }
+        },
+        refusjon: {
+          paakrevd: true,
+          forslag: {
+            perioder: [{ fom: '2023-01-02', tom: '2023-01-02', beloep: 46000.0 }],
+            opphoersdato: '2023-09-30'
+          }
+        }
+      });
+    });
+
+    let typer;
+    act(() => {
+      typer = result.current.arbeidsgiverKreverRefusjon();
+    });
+
+    expect(typer).toBeTruthy();
+  });
+
+  it('should return if arbeidsgiverRefusjonskravHarEndringer', () => {
+    const { result } = renderHook(() => useBoundStore((state) => state));
+
+    act(() => {
+      result.current.initForespurtData({
+        arbeidsgiverperiode: { paakrevd: false },
+        inntekt: {
+          paakrevd: true,
+          forslag: {
+            type: 'ForslagInntektGrunnlag',
+            beregningsmaaneder: ['2023-05', '2023-06', '2023-07']
+          }
+        },
+        refusjon: {
+          paakrevd: true,
+          forslag: {
+            perioder: [{ fom: '2023-01-02', tom: '2023-01-02', beloep: 46000.0 }],
+            opphoersdato: '2023-09-30'
+          }
+        }
+      });
+    });
+
+    let typer;
+    act(() => {
+      typer = result.current.arbeidsgiverRefusjonskravHarEndringer();
+    });
+
+    expect(typer).toBeTruthy();
+  });
+
+  it('should return if arbeidsgiverRefusjonskravOpphører', () => {
+    const { result } = renderHook(() => useBoundStore((state) => state));
+    act(() => {
+      result.current.initForespurtData({
+        arbeidsgiverperiode: { paakrevd: false },
+        inntekt: {
+          paakrevd: true,
+          forslag: {
+            type: 'ForslagInntektGrunnlag',
+            beregningsmaaneder: ['2023-05', '2023-06', '2023-07']
+          }
+        },
+        refusjon: {
+          paakrevd: true,
+          forslag: {
+            perioder: [{ fom: '2023-01-02', tom: '2023-01-02', beloep: 46000.0 }],
+            opphoersdato: '2023-09-30'
+          }
+        }
+      });
+    });
+
+    let typer;
+    act(() => {
+      typer = result.current.arbeidsgiverRefusjonskravOpphører();
+    });
+
+    expect(typer).toBeTruthy();
+  });
 });
