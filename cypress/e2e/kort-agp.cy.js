ad3d2cfa67409a00ee716ce0b08a598dbb2ab08f
Kent Fjellstad <kent.fjellstad@nav.no>
2024-02-05 08:56:55 +0100

    Fjerner kommentarer

--- cypress/e2e/kort-agp.cy.js
+++ cypress/e2e/kort-agp.cy.js
@@ -43,51 +43,15 @@ describe('Utfylling og innsending av skjema', () => {
 
     cy.wait('@trenger');
 
-    // cy.findByRole('group', { name: 'Betaler arbeidsgiver ut full lønn i arbeidsgiverperioden?' })
-    //   .findByLabelText('Ja')
-    //   .check();
-
-    // cy.findByRole('group', { name: 'Betaler arbeidsgiver lønn og krever refusjon etter arbeidsgiverperioden?' })
-    //   .findByLabelText('Ja')
-    //   .check();
-
-    // cy.get('#lus-utbetaling-endring-radio [type="radio"]').last().click();
-    // cy.get('#lus-sluttdato-velg [type="radio"]').last().click();
-
-    // cy.get('[data-cy="endre-belop"]').click();
-
     cy.findByLabelText('Jeg bekrefter at opplysningene jeg har gitt, er riktige og fullstendige.').check();
 
-    // cy.get('[data-cy="inntekt-belop-input"]').clear();
-    // cy.get('[data-cy="inntekt-belop-input"]').type('70000');
-
-    // cy.get('[data-cy="refusjon-arbeidsgiver-belop"]')
-    //   .invoke('text')
-    //   .should('match', /70\s000,00\skr/);
-
-    // cy.get('[data-cy="endre-refusjon-arbeidsgiver-belop"]').click();
-
-    // cy.get('[data-cy="inntekt-belop-input"]').clear().type('75000');
-
-    // cy.get('[data-cy="refusjon-arbeidsgiver-belop-input"]').should('have.value', '75000');
-
-    // cy.findAllByLabelText('Velg endringsårsak').select('Bonus');
-
     cy.findAllByRole('button', { name: /Endre/ }).first().click();
-    // cy.findByRole('button', { name: /Legg til periode/ }).click();
 
-    // cy.findAllByLabelText('Fra').last().clear().type('30.01.23');
-    // cy.realPress('Escape');
     cy.findAllByLabelText('Til').last().clear().type('116.03.23');
     cy.realPress('Escape');
 
     cy.findByLabelText('Utbetalt under arbeidsgiverperiode').clear().type('50000');
     cy.realPress('Escape');
-
-    // cy.wait('@inntektsdata').its('request.body').should('deep.equal', {
-    //   forespoerselId: '12345678-3456-5678-2457-123456789012',
-    //   skjaeringstidspunkt: '2023-01-30'
-    // });
 
     cy.findAllByLabelText('Velg begrunnelse for kort arbeidsgiverperiode').select('Arbeidsforholdet er avsluttet');
 
