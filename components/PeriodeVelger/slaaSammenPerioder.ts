4b0f4e255fe757c023add426697751bac88ff466
Kent Fjellstad <kent.fjellstad@nav.no>
2024-03-21 17:25:32 +0100

    Fjerner toSorted helt

--- components/PeriodeVelger/slaaSammenPerioder.ts
+++ components/PeriodeVelger/slaaSammenPerioder.ts
@@ -9,27 +9,25 @@ export interface SammenslaattPeriode {
 
 export default function slaaSammenPerioder(perioder: { fom: Date; tom: Date; id: string }[]): SammenslaattPeriode[] {
   const perioderSorted = [...perioder].sort((a, b) => (a.fom > b.fom ? -1 : 1));
-  const sammenslaattePerioder = perioder
-    .toSorted((a, b) => (a.fom > b.fom ? -1 : 1))
-    .reduce((acc, periode) => {
-      if (acc.length === 0) {
-        acc.push({ ...periode });
-        return acc;
-      }
-      let last = acc[acc.length - 1];
-      if (acc[acc.length - 1] && acc[acc.length - 1].periode) {
-        last = acc[acc.length - 1].periode[acc[acc.length - 1].periode.length - 1];
-      }
-      if (Math.abs(differenceInCalendarDays(last.fom, periode.tom)) < 16) {
-        if (!acc[acc.length - 1].periode) {
-          acc[acc.length - 1].periode = [{ ...last }, { ...periode }];
-        } else acc[acc.length - 1].periode.push(periode);
-      } else {
-        acc.push({ ...periode });
-      }
-
+  const sammenslaattePerioder = perioderSorted.reduce((acc, periode) => {
+    if (acc.length === 0) {
+      acc.push({ ...periode });
       return acc;
-    }, [] as SammenslaattPeriode[]);
+    }
+    let last = acc[acc.length - 1];
+    if (acc[acc.length - 1] && acc[acc.length - 1].periode) {
+      last = acc[acc.length - 1].periode[acc[acc.length - 1].periode.length - 1];
+    }
+    if (Math.abs(differenceInCalendarDays(last.fom, periode.tom)) < 16) {
+      if (!acc[acc.length - 1].periode) {
+        acc[acc.length - 1].periode = [{ ...last }, { ...periode }];
+      } else acc[acc.length - 1].periode.push(periode);
+    } else {
+      acc.push({ ...periode });
+    }
+
+    return acc;
+  }, [] as SammenslaattPeriode[]);
 
   const gruppertePerioder = sammenslaattePerioder.map((periode) => {
     if (!periode.periode) {
