3a4c53460a2c80b7a1b4531e5363741f5ecbcdef
Kent Fjellstad <kent.fjellstad@nav.no>
2024-06-19 15:22:04 +0200

    Splitter ut hentingen av arbeidsforhold i egen hook

---
+++ utils/useArbeidsforhold.ts
@@ -0,0 +1,35 @@
+import useSWRImmutable from 'swr/immutable';
+import environment from '../config/environment';
+import fetcherArbeidsforhold from './fetcherArbeidsforhold';
+
+export default function useArbeidsforhold(identitetsnummer: string | undefined, backendFeil: any) {
+  return useSWRImmutable(
+    [environment.initierBlankSkjemaUrl, identitetsnummer],
+    ([url, idToken]) => fetcherArbeidsforhold(!!identitetsnummer ? url : null, idToken),
+    {
+      onError: (err) => {
+        console.error('Kunne ikke hente arbeidsforhold', err);
+        if (err.status === 401) {
+          const ingress = window.location.hostname + environment.baseUrl;
+          const currentPath = window.location.href;
+
+          window.location.replace(`https://${ingress}/oauth2/login?redirect=${ingress}/initiering`);
+        }
+
+        if (err.status === 404) {
+          backendFeil.current.push({
+            felt: 'Backend',
+            text: 'Kunne ikke finne arbeidsforhold for personen, sjekk at du har tastet riktig personnummer'
+          });
+        } else if (err.status !== 200) {
+          backendFeil.current.push({
+            felt: 'Backend',
+            text: 'Kunne ikke hente arbeidsforhold'
+          });
+        }
+      },
+      refreshInterval: 0,
+      shouldRetryOnError: false
+    }
+  );
+}
