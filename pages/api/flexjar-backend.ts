34138b26be306119ed3abf5170df43b780664f31
Kent Fjellstad <kent.fjellstad@nav.no>
2024-07-30 07:55:32 +0200

    Rydder opp litt i flexjar innsendingen (#608)
    * Rydder opp litt

---
+++ pages/api/flexjar-backend.ts
@@ -0,0 +1,58 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import httpProxyMiddleware from 'next-http-proxy-middleware';
+
+import handleProxyInit from '../../utils/api/handleProxyInit';
+import { getToken, requestOboToken, validateToken } from '@navikt/oasis';
+
+const basePath = 'http://' + global.process.env.FLEXJAR_URL + '/api/v1/feedback';
+
+export const config = {
+  api: {
+    externalResolver: true,
+    bodyParser: false
+  }
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<unknown>) => {
+  const env = process.env.NODE_ENV;
+  if (env == 'development') {
+    setTimeout(() => {
+      return res.status(201);
+    }, 100);
+  } else if (env == 'production') {
+    const token = getToken(req);
+    if (!token) {
+      /* håndter manglende token */
+      console.error('Mangler token i header');
+      return res.status(401);
+    }
+
+    const validation = await validateToken(token);
+    if (!validation.ok) {
+      console.log('Validering feilet: ', validation.error);
+      return res.status(401);
+    }
+
+    const obo = await requestOboToken(token, process.env.FLEXJAR_BACKEND_CLIENT_ID!);
+    if (!obo.ok) {
+      /* håndter obo-feil */
+      console.error('OBO-feil: ', obo.error);
+      return res.status(401);
+    }
+
+    return httpProxyMiddleware(req, res, {
+      target: basePath,
+      onProxyInit: handleProxyInit,
+      headers: { Authorization: `bearer ${obo.token}` },
+      pathRewrite: [
+        {
+          patternStr: '^/api/flexjar-backend',
+          replaceStr: ''
+        }
+      ]
+    });
+  }
+};
+
+export default handler;
