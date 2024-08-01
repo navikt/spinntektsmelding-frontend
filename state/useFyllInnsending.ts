7fd1eeccc1877810450f824b1a2e3a242ba775b5
Kent Fjellstad <kent.fjellstad@nav.no>
2024-08-01 15:02:30 +0200

    Lint

--- state/useFyllInnsending.ts
+++ state/useFyllInnsending.ts
@@ -197,7 +197,7 @@ export default function useFyllInnsending() {
               endringer: konverterRefusjonEndringer(harRefusjonEndringer, refusjonEndringer)
             }
           : null,
-      avsenderTlf: innsenderTelefonNr || ''
+      avsenderTlf: innsenderTelefonNr ?? ''
     };
 
     return skjemaData;
