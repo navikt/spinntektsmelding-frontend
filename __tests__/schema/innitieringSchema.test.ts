c9f05682d3108611ae120e4fb456895425221c02
Kent Fjellstad <kent.fjellstad@nav.no>
2024-09-17 13:23:38 +0200

    Schema test

---
+++ __tests__/schema/innitieringSchema.test.ts
@@ -0,0 +1,87 @@
+import InitieringSchema from '../../schema/initieringSchema';
+
+import testFnr from '../../mockdata/testFnr';
+import testOrganisasjoner from '../../mockdata/testOrganisasjoner';
+import { z } from 'zod';
+
+describe('InitieringSchema', () => {
+  it('should validate InitieringSchema', () => {
+    const data = {
+      organisasjonsnummer: testOrganisasjoner[0].organizationNumber,
+      fulltNavn: 'Test Testesen',
+      personnummer: testFnr.GyldigeFraDolly.TestPerson1
+    };
+
+    expect(InitieringSchema.safeParse(data).success).toBe(true);
+  });
+
+  it('should validate InitieringSchema with error on fnr', () => {
+    const data = {
+      organisasjonsnummer: testOrganisasjoner[0].organizationNumber,
+      fulltNavn: 'Test Testesen',
+      personnummer: testFnr.Ugyldige.UgyldigKontrollSiffer
+    };
+
+    expect(InitieringSchema.safeParse(data).success).toBe(false);
+    expect(InitieringSchema.safeParse(data).error).toEqual(
+      new z.ZodError([
+        {
+          code: 'custom',
+          message: 'Ugyldig personnummer',
+          path: ['personnummer']
+        }
+      ])
+    );
+  });
+
+  it('should validate InitieringSchema with error on organisasjonsnummer', () => {
+    const data = {
+      organisasjonsnummer: '1234566789',
+      fulltNavn: 'Test Testesen',
+      personnummer: testFnr.GyldigeFraDolly.TestPerson3
+    };
+
+    expect(InitieringSchema.safeParse(data).success).toBe(false);
+    expect(InitieringSchema.safeParse(data).error).toEqual(
+      new z.ZodError([
+        {
+          code: 'too_big',
+          maximum: 9,
+          type: 'string',
+          inclusive: true,
+          exact: false,
+          message: 'Organisasjonsnummeret er for langt, det må være 9 siffer',
+          path: ['organisasjonsnummer']
+        },
+        {
+          code: 'custom',
+          message: 'Velg arbeidsgiver',
+          path: ['organisasjonsnummer']
+        }
+      ])
+    );
+  });
+
+  it('should validate InitieringSchema with error on fulltNavn', () => {
+    const data = {
+      organisasjonsnummer: testOrganisasjoner[0].organizationNumber,
+      fulltNavn: '',
+      personnummer: testFnr.GyldigeFraDolly.TestPerson2
+    };
+
+    expect(InitieringSchema.safeParse(data).success).toBe(false);
+    expect(InitieringSchema.safeParse(data).error).toEqual(
+      new z.ZodError([
+        {
+          code: 'too_small',
+          minimum: 1,
+          type: 'string',
+          inclusive: true,
+          exact: false,
+          message: 'String must contain at least 1 character(s)',
+          path: ['fulltNavn']
+        }
+      ])
+    );
+  });
+});
