cb6d23d778f4412c78a667f4e502752043e58232
Kent Fjellstad <kent.fjellstad@nav.no>
2024-09-26 12:21:21 +0200

    Console.log

--- validators/validerEndringAvMaanedslonn.ts
+++ validators/validerEndringAvMaanedslonn.ts
@@ -28,7 +28,6 @@ export default function valdiderEndringAvMaanedslonn(
     });
     return feilmeldinger;
   }
-  console.log('validerEndringAvMaanedslonn', harRefusjonEndringer, refusjonEndringer, lonnISykefravaeret, sluttdato);
   if (harRefusjonEndringer === 'Ja' && refusjonEndringer) {
     refusjonEndringer.forEach((endring, index) => {
       if (ugyldigEllerNegativtTall(endring.beloep)) {
