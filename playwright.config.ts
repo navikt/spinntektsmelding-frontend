7618d5182df8f194a426c6add87177069b27cea3
Kent Fjellstad <kent.fjellstad@nav.no>
2024-09-27 08:16:20 +0200

    Webserver timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    baseURL: 'http://127.0.0.1:3000/im-dialog',
+    baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry'
   },
 
