fb3e563db914ca9553525451254174d59e4c0a23
Kent Fjellstad <kent.fjellstad@nav.no>
2024-09-27 08:05:27 +0200

    Webserver timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,7 +74,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev:decoratorless',
-    url: 'http://127.0.0.1:3000/im-dialog'
-    // reuseExistingServer: !process.env.CI
+    url: 'http://127.0.0.1:3000/im-dialog',
+    timeout: 120000,
+    reuseExistingServer: !process.env.CI
   }
 });
