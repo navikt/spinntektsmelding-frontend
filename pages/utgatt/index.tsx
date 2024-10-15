31116ff900ba0435b791d1481618f090eedac787
Magnus Sælensminde <41949947+magnusae@users.noreply.github.com>
2024-10-15 13:42:34 +0200

    Opprett "Saken er utgått"-side (#715)
    Når vi setter en sak/oppgave til utgått ønsker vi å linke til en statisk side som forklarer at den er utgått.
    
    Commits:
    * Opprett en statisk side som skal linkes til fra utgåtte saker/oppgaver
    
    * Juster tekst og knappelink
    
    * Fjern ubrukt import

---
+++ pages/utgatt/index.tsx
@@ -0,0 +1,31 @@
+import { BodyShort, Box, Button, Heading, Link, List, Page, VStack } from '@navikt/ds-react';
+import environment from '../../config/environment';
+
+const Utgatt = () => {
+  return (
+    <Page.Block as='main' width='xl' gutters>
+      <Box paddingBlock='20 16'>
+        <VStack gap='16'>
+          <VStack gap='12' align='start'>
+            <div>
+              <Heading level='1' size='large' spacing>
+                Saken er utgått
+              </Heading>
+              <BodyShort>NAV har ikke lenger behov for at du sender inn opplysninger i akkurat denne saken.</BodyShort>
+              <List>
+                <List.Item>
+                  <Link href={environment.saksoversiktUrl}>Gå til saksoversikten for å finne dine saker.</Link>
+                </List.Item>
+              </List>
+            </div>
+            <Button as='a' href={environment.minSideArbeidsgiver}>
+              Gå til Min side
+            </Button>
+          </VStack>
+        </VStack>
+      </Box>
+    </Page.Block>
+  );
+};
+
+export default Utgatt;
