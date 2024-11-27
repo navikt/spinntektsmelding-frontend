333e29b2628efa4954eb76b0dadbb6cd11bd527b
Kent Fjellstad <kent.fjellstad@nav.no>
2024-11-27 14:41:07 +0100

    Valideringstester og oppgradering av yarn (#760)
    * Valideringstester og oppgradering av yarn
    
    * Yaaarn

--- __tests__/validators/validerLonnIArbeidsgiverperioden.test.ts
+++ __tests__/validators/validerLonnIArbeidsgiverperioden.test.ts
@@ -258,4 +258,117 @@ describe('validerLonnIArbeidsgiverperioden', () => {
 
     expect(validerLonnIArbeidsgiverperioden(input, undefined, 1010)).toEqual(expected);
   });
+
+  it('should return an error when status is undefined and agp i missing', () => {
+    const input: LonnIArbeidsgiverperioden = {
+      begrunnelse: 'ArbeidOpphoert',
+      utbetalt: -500
+    };
+
+    const arbeidsgiverperioder = [{ fom: parseIsoDate('2020-01-01'), tom: parseIsoDate('2020-01-01'), id: '1' }];
+
+    const expected = [
+      {
+        code: 'LONN_I_ARBEIDSGIVERPERIODEN_MANGLER',
+        felt: 'lia-radio'
+      }
+    ];
+
+    expect(validerLonnIArbeidsgiverperioden(input, arbeidsgiverperioder, 1010)).toEqual(expected);
+  });
+
+  it('should return an error when status is Nei and begrunnelse is missing', () => {
+    const input: LonnIArbeidsgiverperioden = {
+      status: 'Nei',
+      begrunnelse: '',
+      utbetalt: 500
+    };
+
+    const arbeidsgiverperioder = [{ fom: parseIsoDate('2020-01-01'), tom: parseIsoDate('2020-01-01'), id: '1' }];
+
+    const expected = [
+      {
+        code: 'LONN_I_ARBEIDSGIVERPERIODEN_BEGRUNNELSE',
+        felt: 'agp.redusertLoennIAgp.begrunnelse'
+      }
+    ];
+
+    expect(validerLonnIArbeidsgiverperioden(input, arbeidsgiverperioder, 1010)).toEqual(expected);
+  });
+
+  it('should return an error when status is Nei and begrunnelse is undefined', () => {
+    const input: LonnIArbeidsgiverperioden = {
+      status: 'Nei',
+      begrunnelse: undefined,
+      utbetalt: 500
+    };
+
+    const arbeidsgiverperioder = [{ fom: parseIsoDate('2020-01-01'), tom: parseIsoDate('2020-01-01'), id: '1' }];
+
+    const expected = [
+      {
+        code: 'LONN_I_ARBEIDSGIVERPERIODEN_BEGRUNNELSE',
+        felt: 'agp.redusertLoennIAgp.begrunnelse'
+      }
+    ];
+
+    expect(validerLonnIArbeidsgiverperioden(input, arbeidsgiverperioder, 1010)).toEqual(expected);
+  });
+
+  it('should return an error when status is Nei and utbetalt is negative', () => {
+    const input: LonnIArbeidsgiverperioden = {
+      status: 'Nei',
+      begrunnelse: 'BeskjedGittForSent',
+      utbetalt: -500
+    };
+
+    const arbeidsgiverperioder = [{ fom: parseIsoDate('2020-01-01'), tom: parseIsoDate('2020-01-01'), id: '1' }];
+
+    const expected = [
+      {
+        code: 'LONN_I_ARBEIDSGIVERPERIODEN_BELOP',
+        felt: 'agp.redusertLoennIAgp.beloep'
+      }
+    ];
+
+    expect(validerLonnIArbeidsgiverperioden(input, arbeidsgiverperioder, 1010)).toEqual(expected);
+  });
+
+  it('should return an error when status is Nei and utbetalt is undefined', () => {
+    const input: LonnIArbeidsgiverperioden = {
+      status: 'Nei',
+      begrunnelse: 'BeskjedGittForSent',
+      utbetalt: undefined
+    };
+
+    const arbeidsgiverperioder = [{ fom: parseIsoDate('2020-01-01'), tom: parseIsoDate('2020-01-01'), id: '1' }];
+
+    const expected = [
+      {
+        code: 'LONN_I_ARBEIDSGIVERPERIODEN_BELOP',
+        felt: 'agp.redusertLoennIAgp.beloep'
+      }
+    ];
+
+    expect(validerLonnIArbeidsgiverperioden(input, arbeidsgiverperioder, 1010)).toEqual(expected);
+  });
+
+  it('should return an error when status is Ja and agp is missing', () => {
+    const input: LonnIArbeidsgiverperioden = {
+      status: 'Ja',
+      begrunnelse: 'BeskjedGittForSent',
+      utbetalt: 500
+    };
+
+    const arbeidsgiverperioder = [{ fom: parseIsoDate('2020-01-01'), tom: parseIsoDate('2020-01-01'), id: '1' }];
+
+    const expected = [
+      {
+        code: 'LONN_I_ARBEIDSGIVERPERIODEN_UTEN_ARBEIDSGIVERPERIODE',
+        felt: 'lia-radio'
+      }
+    ];
+
+    expect(validerLonnIArbeidsgiverperioden(input, undefined, 1010)).toEqual(expected);
+  });
 });
