c993db963e8cffc22f700c0c0b327c9c6e1d7995
Kent Fjellstad <kent.fjellstad@nav.no>
2024-11-27 13:33:19 +0100

    Tester som virker

--- utils/fetchKvitteringsdata.ts
+++ utils/fetchKvitteringsdata.ts
@@ -31,18 +31,9 @@ const fetchKvitteringsdata = (
         throw error;
       }
 
-      return res
-        .json()
-        .then((data: MottattKvitteringSchema) => {
-          return { status: res.status, data };
-        })
-        .catch((res) => {
-          const jsonError = new NetworkError('An error occurred while decoding the data.');
-          // Attach extra info to the error object.
-
-          jsonError.status = res.status;
-          throw jsonError;
-        });
+      return res.json().then((data: MottattKvitteringSchema) => {
+        return { status: res.status, data };
+      });
     })
     .catch((errorRes) => {
       const error = new NetworkError('An error occurred while fetching the data.');
