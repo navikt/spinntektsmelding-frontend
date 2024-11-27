d989c6ccd4d28d72cf9d3e765613674dac7fc86b
Kent Fjellstad <kent.fjellstad@nav.no>
2024-11-27 13:49:38 +0100

    Knip (#755)
    * Bruker responseBackendError schema
    
    * Fjerner et par knip feil
    
    * Testdekning

--- __tests__/utils/fetchKvitteringsdata.test.ts
+++ __tests__/utils/fetchKvitteringsdata.test.ts
@@ -1,33 +1,40 @@
-import { http, HttpResponse } from 'msw';
+import { vi } from 'vitest';
 import fetchKvitteringsdata from '../../utils/fetchKvitteringsdata';
+import createFetchMock from 'vitest-fetch-mock';
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
 
 describe('fetchKvitteringsdata', () => {
-  it.skip('returns the fetched data when the request is successful', async () => {
+  beforeEach(() => {
+    fetchMocker.doMock();
+    fetch.resetMocks();
+  });
+
+  it('returns the fetched data when the request is successful', async () => {
     const url = 'http://example.com/api/forespoersel';
     const forespoerselId = '8d50ef20-37b5-4829-ad83-56219e70b375';
 
-    http.get('http://example.com/api/forespoersel/8d50ef20-37b5-4829-ad83-56219e70b375', () => {
-      return new HttpResponse('Data fetched successfully', { status: 200 });
-    });
+    fetch.mockResponseOnce(JSON.stringify('Data fetched successfully', { status: 200 }));
 
     const response = await fetchKvitteringsdata(url, forespoerselId);
 
-    expect(response).toEqual({ status: 200, data: { message: 'Data fetched successfully' } });
+    expect(response).toEqual({ status: 200, data: 'Data fetched successfully' });
   });
 
   it('returns a 404 status and empty data when the request is made with an invalid UUID', async () => {
     const url = '/api/forespoersel';
     const forespoerselId = 'invalid-uuid';
 
+    fetch.mockResponseOnce(null, { status: 404 });
+
     const response = await fetchKvitteringsdata(url, forespoerselId);
 
-    expect(response).toEqual({ status: 404, data: {} });
+    expect(response).toEqual({ status: 404, data: undefined });
   });
 
   it('throws a NetworkError when the request fails', async () => {
-    http.get('http://example.com/api/forespoersel/8d50ef20-37b5-4829-ad83-56219e70b375', () => {
-      return new HttpResponse(null, { status: 500, statusText: 'Internal Server Error' });
-    });
+    fetch.mockResponseOnce(null, { status: 500, statusText: 'Internal Server Error' });
 
     const url = 'http://example.com/api/forespoersel';
     const forespoerselId = '8d50ef20-37b5-4829-ad83-56219e70b375';
@@ -37,16 +44,33 @@ describe('fetchKvitteringsdata', () => {
     );
   });
 
-  it.skip('throws a NetworkError when the response data cannot be decoded', async () => {
+  it('throws a NetworkError when the response data cannot be decoded', async () => {
     const url = 'http://example.com/api/forespoersel';
     const forespoerselId = '8d50ef20-37b5-4829-ad83-56219e70b375';
 
-    http.get('http://example.com/api/forespoersel/8d50ef20-37b5-4829-ad83-56219e70b375', () => {
-      return HttpResponse.json('Stuff went wrong');
-    });
+    fetch.mockResponseOnce('ups, something went wrong');
 
     await expect(fetchKvitteringsdata(url, forespoerselId)).rejects.toThrowError(
-      'An error occurred while decoding the data.'
+      'An error occurred while fetching the data.'
     );
+  });
+
+  it('gives a nice response', async () => {
+    const kvitteringEkstern = {
+      kvitteringEkstern: {
+        avsenderSystem: 'test',
+        referanse: 'test',
+        tidspunkt: '2024-11-27T11:55:38.301Z'
+      }
+    };
+
+    fetch.mockResponseOnce(JSON.stringify(kvitteringEkstern));
+
+    const url = 'http://example.com/api/forespoersel';
+    const forespoerselId = '8d50ef20-37b5-4829-ad83-56219e70b375';
+
+    const result = await fetchKvitteringsdata(url, forespoerselId);
+
+    expect(result).toEqual({ status: 200, data: kvitteringEkstern });
   });
 });
--- utils/fetchKvitteringsdata.ts
+++ utils/fetchKvitteringsdata.ts
@@ -1,9 +1,16 @@
+import mottattKvitteringSchema from '../schema/mottattKvitteringSchema';
 import NetworkError from './NetworkError';
 import isValidUUID from './isValidUUID';
+import { z } from 'zod';
 
-const fetchKvitteringsdata = (url: string, forespoerselId: string) => {
+type MottattKvitteringSchema = z.infer<typeof mottattKvitteringSchema>;
+
+const fetchKvitteringsdata = (
+  url: string,
+  forespoerselId: string
+): Promise<{ status: number; data: MottattKvitteringSchema | undefined }> => {
   if (isValidUUID(forespoerselId) === false) {
-    return Promise.resolve({ status: 404, data: {} });
+    return Promise.resolve({ status: 404, data: undefined });
   }
   return fetch(`${url}/${forespoerselId}`, {
     method: 'GET',
@@ -24,18 +31,9 @@ const fetchKvitteringsdata = (url: string, forespoerselId: string) => {
         throw error;
       }
 
-      return res
-        .json()
-        .then((data) => {
-          return { status: res.status, data };
-        })
-        .catch((res) => {
-          const jsonError = new NetworkError('An error occurred while decoding the data.');
-          // Attach extra info to the error object.
-
-          jsonError.status = res.status;
-          throw jsonError;
-        });
+      return res.json().then((data: MottattKvitteringSchema) => {
+        return { status: res.status, data };
+      });
     })
     .catch((errorRes) => {
       const error = new NetworkError('An error occurred while fetching the data.');
