3ce5964e9b7ffcebe21d58025350536daa2735b5
Kent Fjellstad <kent.fjellstad@nav.no>
2025-01-24 11:11:21 +0100

    Viser feilmelding når vi ikke finner kvittering

--- pages/kvittering/[kvittid].tsx
+++ pages/kvittering/[kvittid].tsx
@@ -43,6 +43,8 @@ import classNames from 'classnames/bind';
 import FlexJarResponse from '../../components/FlexJarResponse/FlexJarResponse';
 import finnBestemmendeFravaersdag from '../../utils/finnBestemmendeFravaersdag';
 import parseIsoDate from '../../utils/parseIsoDate';
+import HentingAvDataFeilet from '../../components/HentingAvDataFeilet';
+import environment from '../../config/environment';
 
 const Kvittering: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({
   kvittid
@@ -53,6 +55,7 @@ const Kvittering: NextPage<InferGetServerSidePropsType<typeof getServerSideProps
   const hentKvitteringsdata = useHentKvitteringsdata();
 
   const bruttoinntekt = useBoundStore((state) => state.bruttoinntekt);
+  const skjemaFeilet = useBoundStore((state) => state.skjemaFeilet);
 
   const lonnISykefravaeret = useBoundStore((state) => state.lonnISykefravaeret);
   const fullLonnIArbeidsgiverPerioden = useBoundStore((state) => state.fullLonnIArbeidsgiverPerioden);
@@ -100,6 +103,10 @@ const Kvittering: NextPage<InferGetServerSidePropsType<typeof getServerSideProps
     } else if (isValidUUID(kvitteringSlug)) {
       router.push(`/endring/${kvitteringSlug}`);
     }
+  };
+
+  const lukkHentingFeiletModal = () => {
+    window.location.href = environment.saksoversiktUrl;
   };
 
   let innsendingstidspunkt =
@@ -306,6 +313,12 @@ const Kvittering: NextPage<InferGetServerSidePropsType<typeof getServerSideProps
             />
           )}
         </div>
+        <HentingAvDataFeilet
+          open={skjemaFeilet}
+          handleCloseModal={lukkHentingFeiletModal}
+          title='Henting av kvitteringen feilet'
+          ariaLabel='Henting av kvittering feilet'
+        />
       </PageContent>
     </div>
   );
--- utils/useHentKvitteringsdata.ts
+++ utils/useHentKvitteringsdata.ts
@@ -4,11 +4,12 @@ import fetchKvitteringsdata from './fetchKvitteringsdata';
 import useHentSkjemadata from './useHentSkjemadata';
 import useKvitteringInit from '../state/useKvitteringInit';
 import { logger } from '@navikt/next-logger';
+import useBoundStore from '../state/useBoundStore';
 
 export default function useHentKvitteringsdata() {
   const initState = useKvitteringInit();
-  const hentSkjemadata = useHentSkjemadata();
   const router = useRouter();
+  const setSkjemaFeilet = useBoundStore((state) => state.setSkjemaFeilet);
 
   return (pathSlug?: string | Array<string>) => {
     if (Array.isArray(pathSlug)) {
@@ -19,15 +20,13 @@ export default function useHentKvitteringsdata() {
       return fetchKvitteringsdata(environment.hentKvitteringUrl, pathSlug)
         .then((skjemadata) => {
           if (skjemadata.status === 404) {
-            return hentSkjemadata(pathSlug).catch((error: any) => {
-              logger.warn('Feil ved henting av skjemadata i useHentKvitteringsdata', error);
-              logger.warn(error);
-            });
+            setSkjemaFeilet();
+            logger.warn('Fant ikke kvittering for ', pathSlug);
           }
           if (skjemadata.data !== undefined) {
             initState(skjemadata.data);
           }
-          router.replace(`/kvittering/${pathSlug}`, undefined, { shallow: true });
+          // router.replace(`/kvittering/${pathSlug}`, undefined);
         })
         .catch((error: any) => {
           if (error.status === 401) {
@@ -38,10 +37,8 @@ export default function useHentKvitteringsdata() {
           }
 
           if (error.status !== 200 && pathSlug) {
-            return hentSkjemadata(pathSlug).catch((error: any) => {
-              logger.warn('Feil ved henting av skjemadata i useHentKvitteringsdata', error);
-              logger.warn(error);
-            });
+            setSkjemaFeilet();
+            logger.warn('Fant ikke kvittering for ', pathSlug);
           }
         });
     }
