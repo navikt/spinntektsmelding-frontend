03ff4095bd683b39e1f309f5a31c75a21ed9ce72
Kent Fjellstad <kent.fjellstad@nav.no>
2025-03-24 11:32:42 +0100

    Ubrukt import

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import type { InferGetServerSidePropsType, NextPage } from 'next';
 import Head from 'next/head';
 
