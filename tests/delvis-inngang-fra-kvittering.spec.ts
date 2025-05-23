746f8ee81bfbab58e4aed6c6c2ccc729e6e8074d
Kent Fjellstad <kent.fjellstad@nav.no>
2025-05-23 15:56:53 +0200

    Flere tester

---
+++ tests/delvis-inngang-fra-kvittering.spec.ts
@@ -0,0 +1,85 @@
+import { test, expect } from '@playwright/test';
+import trengerDelvis from '../mockdata/trenger-delvis.json';
+import inntektData from '../mockdata/inntektData.json';
+import kvitteringData from '../mockdata/kvittering-delvis.json';
+
+test.describe('Delvis skjema - Utfylling og innsending av skjema', () => {
+  test.beforeEach(async ({ page }) => {
+    // stub collect beacon
+    await page.route('**/collect', (r) => r.fulfill({ status: 202, body: 'OK' }));
+    // stub initial API calls
+    trengerDelvis.erBesvart = true;
+    await page.route('*/**/api/hent-forespoersel', (r) =>
+      r.fulfill({ status: 200, body: JSON.stringify(trengerDelvis), contentType: 'application/json' })
+    );
+    await page.route('*/**/api/inntektsdata', (r) =>
+      r.fulfill({ status: 200, body: JSON.stringify(inntektData), contentType: 'application/json' })
+    );
+    await page.route('*/**/api/hentKvittering/**', (r) =>
+      r.fulfill({ status: 200, body: JSON.stringify(kvitteringData), contentType: 'application/json' })
+    );
+    await page.route('*/**/api/innsendingInntektsmelding', (r) =>
+      r.fulfill({ status: 201, body: JSON.stringify({ name: 'Nothing' }), contentType: 'application/json' })
+    );
+    // navigate to receipt page
+    await page.goto('http://localhost:3000/im-dialog/12345678-3456-5678-2457-123456789012');
+    await page.waitForResponse('*/**/api/hent-forespoersel');
+  });
+
+  test('Changes and submit', async ({ page }) => {
+    // verify on receipt page
+    await page.waitForResponse('*/**/api/hentKvittering/12345678-3456-5678-2457-123456789012');
+    await expect(page).toHaveURL(/\/im-dialog\/kvittering\/12345678/);
+    // click first "Endre"
+    await page.getByRole('button', { name: /Endre/ }).first().click();
+    await page.waitForURL('**/im-dialog/12345678-3456-5678-2457-123456789012');
+
+    // update income to 50000
+    await page.getByRole('button', { name: /Endre/ }).nth(1).click();
+
+    await expect(page.locator('label:text("Månedslønn 02.01.2023")')).toHaveValue('65000');
+    await page.fill('label:text("Månedslønn 02.01.2023")', '50000');
+
+    // select change reason
+    await page.getByLabel('Velg endringsårsak').selectOption('Bonus');
+    // choose refusjon = Ja
+    await page
+      .getByRole('group', { name: 'Betaler arbeidsgiver lønn og krever refusjon under sykefraværet?' })
+      .getByLabel('Ja')
+      .check();
+    // confirm phone and checkbox
+    await expect(page.getByLabel('Telefon innsender')).toHaveValue('12345678');
+    await page.getByLabel('Jeg bekrefter at opplysningene jeg har gitt, er riktige og fullstendige.').check();
+
+    // click second "Endre"
+    await page.getByRole('button', { name: /Endre/ }).nth(1).click();
+    // set refusjonsbeløp
+    await page.fill('label:text("Oppgi refusjonsbeløpet per måned")', '50000');
+
+    // submit
+    const reqPromise = page.waitForRequest('*/**/api/innsendingInntektsmelding');
+    await page.getByRole('button', { name: 'Send' }).click();
+    const req = await reqPromise;
+    const body = JSON.parse(req.postData()!);
+    expect(body).toEqual({
+      forespoerselId: '12345678-3456-5678-2457-123456789012',
+      agp: null,
+      inntekt: {
+        beloep: 50000,
+        inntektsdato: '2023-01-02',
+        naturalytelser: [],
+        endringAarsaker: [{ aarsak: 'Bonus' }]
+      },
+      refusjon: { beloepPerMaaned: 50000, sluttdato: null, endringer: [] },
+      avsenderTlf: '12345678'
+    });
+
+    // confirmation page
+    await expect(page.locator('[data-cy="bestemmendefravaersdag"]')).toHaveText(/02.01.2023/);
+    await expect(page.locator('text="Kvittering - innsendt inntektsmelding"')).toBeVisible();
+    await expect(page.locator('text="12345678"')).toBeVisible();
+    await expect(page.locator('text="Bonus"')).toBeVisible();
+    await expect(page.locator('text="50 000,00 kr/måned"').first()).toBeVisible();
+    // await expect(page.locator('text="02.01.2023"')).toBeVisible();
+  });
+});
---
+++ tests/utils/formPage.ts
@@ -0,0 +1,67 @@
+// pageObject.js
+import { Page, Locator } from '@playwright/test';
+
+export class FormPage {
+  // Store page reference for locator
+  private page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  // Custom locator for radio button within a group
+  async getRadioButton(groupName: string, optionLabel: string): Promise<Locator> {
+    // Find the radio group by its name and return the specific radio button by its label
+    // return this.page.locator(`role=group[name="${groupName}"] input[type="radio"][value="${optionLabel}"]`);
+    return this.page.getByRole('group', { name: groupName }).getByLabel(optionLabel);
+  }
+
+  // Example of a method to check a radio button
+  async checkRadioButton(groupName: string, optionLabel: string): Promise<void> {
+    const radioButton = await this.getRadioButton(groupName, optionLabel);
+    await radioButton.check();
+  }
+
+  // Example of a method to uncheck a radio button
+  async uncheckRadioButton(groupName: string, optionLabel: string): Promise<void> {
+    const radioButton = await this.getRadioButton(groupName, optionLabel);
+    await radioButton.uncheck();
+  }
+
+  async fillInput(label: string, value: string): Promise<void> {
+    const inputField = this.page.getByLabel(label);
+    await inputField.fill(value);
+  }
+  async clickButton(buttonText: string): Promise<void> {
+    const button = this.page.getByRole('button', { name: buttonText });
+    await button.click();
+  }
+  async getInputValue(label: string): Promise<string> {
+    const inputField = this.page.getByLabel(label);
+    return await inputField.inputValue();
+  }
+  async getTextContent(selector: string): Promise<string | null> {
+    const element = this.page.locator(selector);
+    return await element.textContent();
+  }
+  async checkCheckbox(label: string): Promise<void> {
+    const checkbox = this.page.getByLabel(label);
+    await checkbox.check();
+  }
+  async uncheckCheckbox(label: string): Promise<void> {
+    const checkbox = this.page.getByLabel(label);
+    await checkbox.uncheck();
+  }
+  async selectOption(label: string, value: string): Promise<void> {
+    const select = this.page.getByLabel(label);
+    await select.selectOption(value);
+  }
+  async getSelectedOption(label: string): Promise<string> {
+    const select = this.page.getByLabel(label);
+    const selectedOption = await select.inputValue();
+    return selectedOption;
+  }
+  async getByText(text: string): Promise<Locator> {
+    return this.page.locator('text="' + text + '"');
+  }
+}
