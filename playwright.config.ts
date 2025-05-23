48ed0aa716c417dba44049a86af2904b85c6536a
Kent Fjellstad <kent.fjellstad@nav.no>
2025-05-23 09:15:40 +0200

    Config

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,12 +68,14 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ]
+  ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn dev:decoratorless',
+    // url: 'http://localhost:3000',
+    port: 3000,
+    timeout: 120000,
+    reuseExistingServer: !process.env.CI
+  }
 });
