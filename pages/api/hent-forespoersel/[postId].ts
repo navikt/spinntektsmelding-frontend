1dba6718e3e0632284c7a0bee9169fa19baf7fdb
Kent Fjellstad <kent.fjellstad@nav.no>
2025-06-19 10:54:56 +0200

    Lagt til Ansatt har ikke hatt fravær fra jobb som begrunnelse for kort agp (#984)

--- pages/api/hent-forespoersel/[postId].ts
+++ pages/api/hent-forespoersel/[postId].ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import httpProxyMiddleware from 'next-http-proxy-middleware';
 import environment from '../../../config/environment';
 
-import org from '../../../mockdata/trenger-refusjon.json';
+import org from '../../../mockdata/trenger-originalen.json';
 import handleProxyInit from '../../../utils/api/handleProxyInit';
 
 const basePath = 'http://' + global.process.env.IM_API_URI + environment.inntektsmeldingUuidAPI;
