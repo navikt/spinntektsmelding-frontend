5285643294f56d39416cc0061bc73e9ce1376be1
Kent Fjellstad <kent.fjellstad@nav.no>
2025-06-20 07:10:37 +0200

    Advarsler (#986)
    * Legger til advarsler om at dato mangler

--- schema/ApiNaturalytelserSchema.ts
+++ schema/ApiNaturalytelserSchema.ts
@@ -6,7 +6,12 @@ export const ApiNaturalytelserSchema = z.union([
     z.object({
       naturalytelse: NaturalytelseEnumSchema,
       verdiBeloep: z.number().min(0),
-      sluttdato: z.string().date()
+      sluttdato: z
+        .string({
+          required_error: 'Sluttdato mangler',
+          invalid_type_error: 'Ugyldig sluttdato'
+        })
+        .date()
     })
   ),
   z.tuple([])
---
+++ schema/MottattArbeidsgiverSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const MottattArbeidsgiverSchema = z.object({
+  name: z.string(),
+  type: z.string(), // e.g. 'Enterprise'
+  parentOrganizationNumber: z.string().nullable(),
+  organizationForm: z.string(), // e.g. 'KOMM'
+  organizationNumber: z.string(), // e.g. '810007672'
+  socialSecurityNumber: z.string().nullable(),
+  status: z.string() // e.g. 'Active'
+});
+
+export type MottattArbeidsgiver = z.infer<typeof MottattArbeidsgiverSchema>;
