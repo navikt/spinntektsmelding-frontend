ed2ef93ef8a25d68a7e7d27c964b568fc45f8afa
Kent Fjellstad <kent.fjellstad@nav.no>
2025-07-04 14:37:44 +0200

    Duplisert kode

--- schema/ApiPeriodeSchema.ts
+++ schema/ApiPeriodeSchema.ts
@@ -1,6 +1,14 @@
 import { isDate } from 'date-fns';
 import { z } from 'zod';
 
+function isValidISODateString(dateString: string): boolean {
+  if (!dateString) return false;
+  if (dateString.length !== 10) return false;
+  if (isNaN(Date.parse(dateString))) return false;
+  const date = new Date(dateString);
+  return !isNaN(date.getTime()) && isDate(date);
+}
+
 export const ApiPeriodeSchema = z
   .object({
     fom: z
@@ -8,35 +16,13 @@ export const ApiPeriodeSchema = z
         required_error: 'Vennligst fyll inn fra dato',
         invalid_type_error: 'Dette er ikke en dato'
       })
-      .refine(
-        (val) => {
-          if (!val) return false;
-          if (val.length < 10) return false;
-          if (val.length > 10) return false;
-          if (isNaN(Date.parse(val))) return false;
-          if (!isDate(new Date(val))) return false;
-          const date = new Date(val);
-          return !isNaN(date.getTime());
-        },
-        { message: 'Ugyldig dato' }
-      ),
+      .refine((val) => isValidISODateString(val), { message: 'Ugyldig dato' }),
     tom: z
       .string({
         required_error: 'Vennligst fyll inn til dato',
         invalid_type_error: 'Dette er ikke en dato'
       })
-      .refine(
-        (val) => {
-          if (!val) return false;
-          if (val.length < 10) return false;
-          if (val.length > 10) return false;
-          if (isNaN(Date.parse(val))) return false;
-          if (!isDate(new Date(val))) return false;
-          const date = new Date(val);
-          return !isNaN(date.getTime());
-        },
-        { message: 'Ugyldig dato' }
-      )
+      .refine((val) => isValidISODateString(val), { message: 'Ugyldig dato' })
   })
   .refine((val) => !val.fom || !val.tom || val.fom <= val.tom, {
     message: 'Fra dato må være før til dato',
