e76c118bc1b6f53e372d650e29cd1adf360fcc69
Kent Fjellstad <kent.fjellstad@nav.no>
2025-07-04 13:27:13 +0200

    Validering av perioder og feilmeldinger på agp

--- schema/ApiPeriodeSchema.ts
+++ schema/ApiPeriodeSchema.ts
@@ -7,12 +7,24 @@ export const ApiPeriodeSchema = z
         required_error: 'Vennligst fyll inn fra dato',
         invalid_type_error: 'Dette er ikke en dato'
       })
-      .date(),
+      .refine(
+        (val) => {
+          const date = new Date(val);
+          return !isNaN(date.getTime());
+        },
+        { message: 'Ugyldig dato' }
+      ),
     tom: z
       .string({
         required_error: 'Vennligst fyll inn til dato',
         invalid_type_error: 'Dette er ikke en dato'
       })
-      .date()
+      .refine(
+        (val) => {
+          const date = new Date(val);
+          return !isNaN(date.getTime());
+        },
+        { message: 'Ugyldig dato' }
+      )
   })
   .refine((val) => val.fom <= val.tom, { message: 'Fra dato må være før til dato', path: ['fom'] });
