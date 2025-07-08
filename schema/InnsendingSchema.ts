86ec701068a2ad43c532557818a0bbbc28b0c449
Kent Fjellstad <kent.fjellstad@nav.no>
2025-07-08 08:28:11 +0200

    Litt enklere navn på funksjon. Ikke muterende sort

--- schema/InnsendingSchema.ts
+++ schema/InnsendingSchema.ts
@@ -7,7 +7,7 @@ import { BegrunnelseRedusertLoennIAgp } from './BegrunnelseRedusertLoennIAgpSche
 import { ApiNaturalytelserSchema } from './ApiNaturalytelserSchema';
 import { isBefore } from 'date-fns';
 
-function kalkulerAntallDagerIMellom(fom: string, tom: string): number {
+function beregnTotaltAntallDager(fom: string, tom: string): number {
   const fomDate = new Date(fom);
   const tomDate = new Date(tom);
   return Math.ceil((tomDate.getTime() - fomDate.getTime()) / (1000 * 60 * 60 * 24)) + 1; // +1 for å inkludere både start- og sluttdag
@@ -15,7 +15,7 @@ function kalkulerAntallDagerIMellom(fom: string, tom: string): number {
 
 function langtGapIPerioder(perioder: Array<{ fom: string; tom: string }>): boolean {
   if (perioder.length < 2) return false;
-  const sortedPerioder = perioder.sort((a, b) => new Date(a.fom).getTime() - new Date(b.fom).getTime());
+  const sortedPerioder = [...perioder].sort((a, b) => new Date(a.fom).getTime() - new Date(b.fom).getTime());
   for (let i = 1; i < sortedPerioder.length; i++) {
     const gap = new Date(sortedPerioder[i].fom).getTime() - new Date(sortedPerioder[i - 1].tom).getTime();
     if (gap > 16 * 24 * 60 * 60 * 1000) {
@@ -42,7 +42,7 @@ function perioderHarOverlapp(perioder: Array<{ fom: string; tom: string }>): boo
 
 function perioderErOver16dagerTotalt(perioder: Array<{ fom: string; tom: string }>): boolean {
   const totaltAntallDager = perioder.reduce((current: number, periode: { fom: string; tom: string }) => {
-    return kalkulerAntallDagerIMellom(periode.fom, periode.tom) + current;
+    return beregnTotaltAntallDager(periode.fom, periode.tom) + current;
   }, 0);
 
   if (totaltAntallDager > 16) {
@@ -53,7 +53,7 @@ function perioderErOver16dagerTotalt(perioder: Array<{ fom: string; tom: string 
 
 function perioderErUnder16dagerTotalt(perioder: Array<{ fom: string; tom: string }>): boolean {
   const totaltAntallDager = perioder.reduce((current: number, periode: { fom: string; tom: string }) => {
-    return kalkulerAntallDagerIMellom(periode.fom, periode.tom) + current;
+    return beregnTotaltAntallDager(periode.fom, periode.tom) + current;
   }, 0);
 
   if (totaltAntallDager < 16) {
