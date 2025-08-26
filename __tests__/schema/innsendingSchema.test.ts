61dae471c7b00b2d993e55cfd2a73652aa9c2118
Kent Fjellstad <kent.fjellstad@nav.no>
2025-08-26 14:18:03 +0200

    Et par tester

--- __tests__/schema/innsendingSchema.test.ts
+++ __tests__/schema/innsendingSchema.test.ts
@@ -1216,4 +1216,87 @@ describe('InnsendingSchema', () => {
       }
     ]);
   });
+
+  it('should validate InnsendingSchema and give an error if inntekt.beloep is < 0', () => {
+    const data = {
+      agp: {
+        perioder: [{ fom: '2023-02-17', tom: '2023-03-04' }],
+        egenmeldinger: [{ fom: '2023-02-17', tom: '2023-02-19' }],
+        redusertLoennIAgp: { beloep: 99999, begrunnelse: 'StreikEllerLockout' }
+      },
+      inntekt: {
+        beloep: -500000,
+        inntektsdato: '2023-02-14',
+        naturalytelser: [],
+        endringAarsak: { aarsak: 'Bonus' },
+        endringAarsaker: [{ aarsak: 'Bonus' }]
+      },
+      refusjon: null,
+      vedtaksperiodeId: '8d50ef20-37b5-4829-ad83-56219e70b375',
+      sykmeldtFnr: '25087327879',
+      avsender: { orgnr: '911206722', tlf: '12345678' },
+      sykmeldingsperioder: [
+        { fom: '2023-02-20', tom: '2023-03-03' },
+        { fom: '2023-03-05', tom: '2023-03-06' }
+      ]
+    };
+
+    expect(InnsendingSchema.safeParse(data).success).toBe(false);
+
+    expect(InnsendingSchema.safeParse(data).error?.issues).toEqual([
+      {
+        code: 'too_small',
+        inclusive: true,
+        message: 'Månedsinntekt må være større enn eller lik 0',
+        minimum: 0,
+        origin: 'number',
+        path: ['inntekt', 'beloep']
+      }
+    ]);
+  });
+
+  it('should validate InnsendingSchema with error on refusjon < 0', () => {
+    const data = {
+      agp: {
+        perioder: [{ fom: '2023-02-17', tom: '2023-03-04' }],
+        egenmeldinger: [{ fom: '2023-02-17', tom: '2023-02-19' }],
+        redusertLoennIAgp: { beloep: 50001, begrunnelse: 'StreikEllerLockout' }
+      },
+      inntekt: {
+        beloep: 50000,
+        inntektsdato: '2023-02-14',
+        naturalytelser: [],
+        endringAarsak: { aarsak: 'Bonus' },
+        endringAarsaker: [{ aarsak: 'Bonus' }]
+      },
+      refusjon: { beloepPerMaaned: -1000, endringer: [] },
+      vedtaksperiodeId: '8d50ef20-37b5-4829-ad83-56219e70b375',
+      sykmeldtFnr: '25087327879',
+      avsender: { orgnr: '911206722', tlf: '12345678' },
+      sykmeldingsperioder: [
+        { fom: '2023-02-20', tom: '2023-03-03' },
+        { fom: '2023-03-05', tom: '2023-03-06' }
+      ]
+    };
+
+    const result = InnsendingSchema.safeParse(data);
+
+    expect(result.success).toBe(false);
+    expect(result.error?.issues).toEqual([
+      {
+        code: 'too_small',
+        inclusive: true,
+        message: 'Refusjonsbeløpet må være større enn eller lik 0',
+        minimum: 0,
+        origin: 'number',
+        path: ['refusjon', 'beloepPerMaaned']
+      },
+      {
+        code: 'invalid_type',
+        expected: 'string',
+        message: 'Vennligst fyll inn til dato',
+        path: ['refusjon', 'sluttdato']
+      }
+    ]);
+  });
 });
