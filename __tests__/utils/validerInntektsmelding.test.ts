7893051b33160cb15c3a04fdd391b98f4efda02a
Kent Fjellstad <kent.fjellstad@nav.no>
2025-09-23 14:20:20 +0200

    Strammer inn hvilke dager som er forlengelser og tester dette (#1204)
    * Strammer inn hvilke dager som er forlengelser og tester dette
    
    * Sonar
    
    * Tester
    
    * Litt Sonar

--- __tests__/utils/validerInntektsmelding.test.ts
+++ __tests__/utils/validerInntektsmelding.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, expectTypeOf } from 'vitest';
-import type { ValiderResultat, ValiderTekster } from '../../utils/validerInntektsmelding';
+import { ErrorCodes, ValiderResultat, ValiderTekster } from '../../utils/validerInntektsmelding';
 // import type { BruttoinntektFeilkode } from '../../utils/validators/validerBruttoinntekt';
 import type { PeriodeFeilkode } from '../../validators/validerPeriode';
 import type { FullLonnIArbeidsgiverPerioden } from '../../validators/validerFullLonnIArbeidsgiverPerioden';
@@ -19,32 +19,33 @@ import type { BruttoinntektFeilkode } from '../../validators/validerBruttoinntek
 
 describe('validerInntektsmelding type contracts', () => {
   it('ValiderTekster has required string fields', () => {
-    expectTypeOf<ValiderTekster>().toMatchTypeOf<{ felt: string; text: string }>();
+    expectTypeOf<ValiderTekster>().toExtend<{ felt: string; text: string }>();
   });
 
   it('ValiderResultat has correct field types', () => {
-    expectTypeOf<ValiderResultat['felt']>().toMatchTypeOf<string>();
+    expectTypeOf<ValiderResultat['felt']>().toExtend<string>();
 
     // code should accept each union member type
-    expectTypeOf<PeriodeFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<BruttoinntektFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<FullLonnIArbeidsgiverPerioden>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<FullLonnISykefravaeret>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<NaturalytelserFeilkoder>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<LonnIArbeidsgiverperiodenFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<LonnUnderSykefravaeretFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<BekreftOpplysningerFeilkoder>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<EndringAvMaanedslonnFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<PeriodeEgenmeldingFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<PeriodeFravaerFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<TelefonFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<PeriodeOverlappFeilkode>().toMatchTypeOf<ValiderResultat['code']>();
+    expectTypeOf<PeriodeFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<BruttoinntektFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<FullLonnIArbeidsgiverPerioden>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<FullLonnISykefravaeret>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<NaturalytelserFeilkoder>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<LonnIArbeidsgiverperiodenFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<LonnUnderSykefravaeretFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<BekreftOpplysningerFeilkoder>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<EndringAvMaanedslonnFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<PeriodeEgenmeldingFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<PeriodeFravaerFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<TelefonFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<PeriodeOverlappFeilkode>().toExtend<ValiderResultat['code']>();
+    expectTypeOf<ErrorCodes>().toExtend<ValiderResultat['code']>();
 
     // code should also accept the ErrorCodes string literals
-    expectTypeOf<'INGEN_ARBEIDSFORHOLD'>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<'INGEN_FRAVAERSPERIODER'>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<'INGEN_FULL_LONN_I_ARBEIDSGIVERPERIODEN'>().toMatchTypeOf<ValiderResultat['code']>();
-    expectTypeOf<'INGEN_LONN_I_SYKEFRAVAERET'>().toMatchTypeOf<ValiderResultat['code']>();
+    // expectTypeOf<'INGEN_ARBEIDSFORHOLD'>().toExtend<ValiderResultat['code']>();
+    // expectTypeOf<'INGEN_FRAVAERSPERIODER'>().toExtend<ValiderResultat['code']>();
+    // expectTypeOf<'INGEN_FULL_LONN_I_ARBEIDSGIVERPERIODEN'>().toExtend<ValiderResultat['code']>();
+    // expectTypeOf<'INGEN_LONN_I_SYKEFRAVAERET'>().toExtend<ValiderResultat['code']>();
   });
 
   it('accepts a valid ValiderResultat example with ErrorCodes value', () => {
@@ -54,5 +55,53 @@ describe('validerInntektsmelding type contracts', () => {
     };
     expect(result.felt).toBe('someField');
     expect(result.code).toBe('INGEN_ARBEIDSFORHOLD');
+  });
+});
+
+function buildResult(code: ErrorCodes): ValiderResultat {
+  return {
+    felt: 'dummy-felt',
+    code
+  };
+}
+
+describe('ErrorCodes enum', () => {
+  const expectedEntries: [keyof typeof ErrorCodes, string][] = [
+    ['INGEN_ARBEIDSFORHOLD', 'INGEN_ARBEIDSFORHOLD'],
+    ['INGEN_FRAVAERSPERIODER', 'INGEN_FRAVAERSPERIODER'],
+    ['INGEN_FULL_LONN_I_ARBEIDSGIVERPERIODEN', 'INGEN_FULL_LONN_I_ARBEIDSGIVERPERIODEN'],
+    ['INGEN_LONN_I_SYKEFRAVAERET', 'INGEN_LONN_I_SYKEFRAVAERET']
+  ];
+
+  it('har eksakt de forventede enum-key/value-parene', () => {
+    // Sjekk keys
+    expect(Object.keys(ErrorCodes).sort()).toEqual(expectedEntries.map(([k]) => k).sort());
+    // Sjekk values
+    expect(Object.values(ErrorCodes).sort()).toEqual(expectedEntries.map(([, v]) => v).sort());
+  });
+
+  it('hver enumverdi kan brukes i et ValiderResultat (type codeUnion)', () => {
+    expectedEntries.forEach(([, value]) => {
+      const code = value as ErrorCodes;
+      const resultat = buildResult(code);
+      expect(resultat.code).toBe(code);
+    });
+  });
+
+  it('inneholder ingen duplikate values', () => {
+    const values = Object.values(ErrorCodes);
+    const unique = new Set(values);
+    expect(unique.size).toBe(values.length);
+  });
+
+  it('matcher snapshot av enum-objektet (fanger utilsiktede endringer)', () => {
+    expect(ErrorCodes).toMatchInlineSnapshot(`
+      {
+        "INGEN_ARBEIDSFORHOLD": "INGEN_ARBEIDSFORHOLD",
+        "INGEN_FRAVAERSPERIODER": "INGEN_FRAVAERSPERIODER",
+        "INGEN_FULL_LONN_I_ARBEIDSGIVERPERIODEN": "INGEN_FULL_LONN_I_ARBEIDSGIVERPERIODEN",
+        "INGEN_LONN_I_SYKEFRAVAERET": "INGEN_LONN_I_SYKEFRAVAERET",
+      }
+    `);
   });
 });
