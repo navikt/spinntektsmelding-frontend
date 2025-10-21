50d74c91ce677e6661d28021b4e5e0c12a82f0c3
Kent Fjellstad <kent.fjellstad@nav.no>
2025-10-21 14:13:43 +0200

    Ubrukt import

--- pages/api/inntekt-selvbestemt.ts
+++ pages/api/inntekt-selvbestemt.ts
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import httpProxyMiddleware from 'next-http-proxy-middleware';
-import environment from '../../config/environment';
 
 import org from '../../mockdata/inntektData.json';
 import handleProxyInit from '../../utils/api/handleProxyInit';
