4e1d657b7ba1109b082f8ed0d03b153b606f6c40
Kent Fjellstad <kent.fjellstad@nav.no>
2026-01-06 10:10:54 +0100

    Øker testdekningen på Heading3

--- __tests__/components/Heading3.test.tsx
+++ __tests__/components/Heading3.test.tsx
@@ -24,4 +24,41 @@ describe('Heading3', () => {
     expect(HeadingTitle).toBeInTheDocument();
     expect(HeadingTitle).toHaveClass('Test');
   });
+
+  it('renders without padding when unPadded is true', () => {
+    render(<Heading3 unPadded>Innholdstekst</Heading3>);
+
+    const HeadingTitle = screen.getByRole('heading', {
+      level: 3,
+      name: /Innholdstekst/i
+    });
+
+    expect(HeadingTitle).toBeInTheDocument();
+  });
+
+  it('renders with top padding when topPadded is true', () => {
+    render(<Heading3 topPadded>Innholdstekst</Heading3>);
+
+    const HeadingTitle = screen.getByRole('heading', {
+      level: 3,
+      name: /Innholdstekst/i
+    });
+
+    expect(HeadingTitle).toBeInTheDocument();
+  });
+
+  it('renders with both unPadded and topPadded props', () => {
+    render(
+      <Heading3 unPadded topPadded>
+        Innholdstekst
+      </Heading3>
+    );
+
+    const HeadingTitle = screen.getByRole('heading', {
+      level: 3,
+      name: /Innholdstekst/i
+    });
+
+    expect(HeadingTitle).toBeInTheDocument();
+  });
 });
