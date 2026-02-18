62e82b56f93c079fe0bc06f54fcaeaa935400bd7
Kent Fjellstad <kent.fjellstad@nav.no>
2026-02-18 14:22:49 +0100

    Playwright test for å dekke problemet

--- tests/kun-refusjon.spec.ts
+++ tests/kun-refusjon.spec.ts
@@ -169,4 +169,63 @@ test.describe('Utfylling og innsending av skjema – refusjon', () => {
     await expect(page).toHaveURL(`/im-dialog/kvittering/${uuid}?fromSubmit=true`);
     await expect(page.locator('text="Kvittering - innsendt inntektsmelding"')).toBeVisible();
   });
+
+  test('submit inntektsmelding that only requires refusjon, change refusjonsdata - one refusjon endring and beløp is 0', async ({
+    page
+  }) => {
+    const formPage = new FormPage(page);
+
+    await formPage.fillInput('Telefon innsender', '12345678');
+
+    // select refusjon under sykefravær
+    await formPage.checkRadioButton('Betaler arbeidsgiver lønn og krever refusjon under sykefraværet?', 'Ja');
+
+    await formPage.checkRadioButton(
+      'Er det endringer i refusjonsbeløpet eller skal refusjonen opphøre i perioden?',
+      'Ja'
+    );
+
+    await formPage.clickButton('Endre', 1); // click the second "Endre" button to open the change form
+
+    await formPage.fillInput('Oppgi refusjonsbeløpet per måned', '50000');
+
+    // confirm checkbox
+    await formPage.checkCheckbox('Jeg bekrefter at opplysningene jeg har gitt, er riktige og fullstendige.');
+
+    await formPage.fillInput('Endret beløp/måned', '0');
+
+    await formPage.fillInput('Dato for endring', '01.06.25');
+
+    // submit
+
+    // assert request payload
+    const reqPromise = page.waitForRequest('*/**/api/innsendingInntektsmelding');
+    await formPage.clickButton('Send');
+    const req = await reqPromise;
+    const body = JSON.parse(req.postData()!);
+    expect(body).toEqual({
+      forespoerselId: uuid,
+      agp: null,
+      inntekt: null,
+      refusjon: {
+        beloepPerMaaned: 50000,
+        endringer: [
+          {
+            beloep: 0,
+            startdato: '2025-06-01'
+          }
+        ],
+        sluttdato: null
+      },
+      avsenderTlf: '12345678',
+      naturalytelser: []
+    });
+
+    // verify receipt page
+    await page.waitForURL(`/im-dialog/kvittering/${uuid}?fromSubmit=true`);
+    await expect(page).toHaveURL(`/im-dialog/kvittering/${uuid}?fromSubmit=true`);
+    await formPage.assertVisibleTextAtLeastOnce('Kvittering - innsendt inntektsmelding');
+    await formPage.assertVisibleTextAtLeastOnce('Endret refusjonsbeløp');
+    await formPage.assertVisibleTextAtLeastOnce('01.06.2025');
+  });
 });
