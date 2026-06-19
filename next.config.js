8f471f62fa317c703ac6cff7faa60c47650d790d
Kent Fjellstad <kent.fjellstad@nav.no>
2026-06-19 10:28:37 +0200

    Prøver litt config justering

--- next.config.js
+++ next.config.js
@@ -55,7 +55,18 @@ const nextConfig = {
     optimizePackageImports: ['@navikt/aksel-icons', '@navikt/ds-react', 'date-fns', '@grafana/faro-web-sdk']
   },
   outputFileTracingIncludes: {
-    '/*': ['./node_modules/pino-socket/**/*', './node_modules/.pnpm/pino-socket@*/node_modules/pino-socket/**/*']
+    '/*': [
+      './node_modules/pino-socket/**/*',
+      './node_modules/.pnpm/pino-socket@*/node_modules/pino-socket/**/*',
+      './node_modules/backoff/**/*',
+      './node_modules/.pnpm/backoff@*/node_modules/backoff/**/*',
+      './node_modules/nopt/**/*',
+      './node_modules/.pnpm/nopt@*/node_modules/nopt/**/*',
+      './node_modules/pump/**/*',
+      './node_modules/.pnpm/pump@*/node_modules/pump/**/*',
+      './node_modules/through2/**/*',
+      './node_modules/.pnpm/through2@*/node_modules/through2/**/*'
+    ]
   },
   env: {
     NEXT_PUBLIC_APP_VERSION: version
