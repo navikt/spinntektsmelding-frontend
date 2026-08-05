420eebb89df491a0deb6d9d01d7e2e38e75da830
Kent Fjellstad <kent.fjellstad@nav.no>
2026-08-05 12:40:43 +0200

    Strammer inn på formatene telefonnummrene kan ha (#1715)
    * Strammer inn på formatene telefonnummrene kan ha
    
    * Tester og samkjøring av kode

--- validators/validerTelefon.ts
+++ validators/validerTelefon.ts
@@ -1,3 +1,4 @@
+import { isTlfNumber } from '../utils/isTlfNumber';
 import { ValiderResultat } from '../utils/validerInntektsmelding';
 
 export enum TelefonFeilkode {
@@ -14,8 +15,7 @@ export default function validerTelefon(telefon?: string): Array<ValiderResultat>
       code: TelefonFeilkode.TELEFON_MANGLER
     });
   } else {
-    const telefonRegex = /^(\+\d{10}|00\d{10}|\d{8})$/;
-    const validTelefon = telefonRegex.test(telefon);
+    const validTelefon = isTlfNumber(telefon);
 
     if (!validTelefon) {
       feilkoder.push({
